da442f81b64209111bdbbc247082f5280fb80a84
v_vflqiu <v_vflqiu@tencent.com>
2020-12-11 22:10:28 +0800

    Update 新增压缩JS CSS

--- scripts/webpack.config.prod.js
+++ scripts/webpack.config.prod.js
@@ -1,1 +1,23 @@
-module.exports = {}
+const { merge } = require('webpack-merge');
+const WebpakBaseConfig = require('./webpack.config.base');
+// 压缩JS
+const TerserWwebpackPlugin = require('terser-webpack-plugin');
+// 压缩CSS
+const OptimizeCssAssets = require('optimize-css-assets-webpack-plugin');
+
+module.exports = merge(WebpakBaseConfig, {
+  devtool: 'source-map',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserWwebpackPlugin({
+        parallel: true, // 多线程
+      }),
+    ],
+  },
+  plugins: [
+    new OptimizeCssAssets({
+      assetNameRegExp: /.optimize.css$/g,
+    })
+  ]
+});
