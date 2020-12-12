192c13fcfb76baea26d05dc3945b6595087ce1d1
One-Lemon <qq982516861@gmail.com>
2020-12-12 13:57:49 +0800

    Update webpack cli over

--- scripts/webpack.config.dev.js
+++ scripts/webpack.config.dev.js
@@ -1,14 +1,28 @@
-const { merge } = require("webpack-merge");
-const Path = require('path');
-const WebpackBaseConfig = require('./webpack.config.base');
+const WebpackBaseConfig = require("./webpack.config.base")
+// 代码分析报告
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer")
+const { merge } = require("webpack-merge")
+const Path = require("path")
 
+const isBundle = process.env.NODE_ENV === "bundle"
 module.exports = merge(WebpackBaseConfig, {
-  devtool: 'eval',
-  devServer: {
-    port: 7777,
-    host: 'localhost',
-    hot: true,
-    contentBase: Path.join(__dirname, '../dist/'),
-    overlay: true,
-  }
+	devtool: "eval-source-map",
+	devServer: {
+		port: 7777,
+		host: "localhost",
+		hot: true,
+		contentBase: Path.join(__dirname, "../dist/"),
+		overlay: true,
+	},
+	plugins: [
+		new BundleAnalyzerPlugin({
+			analyzerMode: isBundle ? "static" : "disabled",
+			generateStatsFile: false,
+		}),
+	],
+	// 减少体积 提高性能
+	optimization: {
+		runtimeChunk: true,
+	},
+	stats: "errors-warnings",
 })
--- scripts/webpack.config.prod.js
+++ scripts/webpack.config.prod.js
@@ -1,23 +1,29 @@
-const { merge } = require('webpack-merge');
-const WebpakBaseConfig = require('./webpack.config.base');
+const { merge } = require("webpack-merge")
+const WebpakBaseConfig = require("./webpack.config.base")
 // 压缩JS
-const TerserWwebpackPlugin = require('terser-webpack-plugin');
+const TerserWwebpackPlugin = require("terser-webpack-plugin")
 // 压缩CSS
-const OptimizeCssAssets = require('optimize-css-assets-webpack-plugin');
+const OptimizeCssAssets = require("optimize-css-assets-webpack-plugin")
 
 module.exports = merge(WebpakBaseConfig, {
-  devtool: 'source-map',
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserWwebpackPlugin({
-        parallel: true, // 多线程
-      }),
-    ],
-  },
-  plugins: [
-    new OptimizeCssAssets({
-      assetNameRegExp: /.optimize.css$/g,
-    })
-  ]
-});
+	devtool: "source-map",
+	optimization: {
+		minimizer: [
+			new TerserWwebpackPlugin({
+				parallel: true, // 多线程
+			}),
+		],
+	},
+	plugins: [
+		new OptimizeCssAssets({
+			assetNameRegExp: /\.css\.*(?!.*map)/g,
+			cssProcessorOptions: {
+				discardComments: { removeAll: true },
+				safe: true,
+				autoprefixer: {
+					disabled: true,
+				},
+			},
+		}),
+	],
+})
