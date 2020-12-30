22f09d1276fcc7507b84e849ac34cef6b712151e
v_vflqiu <v_vflqiu@tencent.com>
2020-12-30 18:24:50 +0800

    Update card

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,8 +1,42 @@
 // card-item
 import React from 'react';
+import { Image, Statistic } from 'antd';
+import { FireOutlined, HeartOutlined, DownloadOutlined } from '@ant-design/icons';
+import './index.less';
 
-const Card: React.FC = () => {
-    return <div>sad</div>;
+interface IProps {
+    imgUrl?: string;
+    title?: string | number;
+    subTitle?: string | number;
+    views?: number;
+    forwards?: number;
+    loves?: number;
+}
+
+const Card: React.FC<IProps> = ({ imgUrl, title, subTitle, views = 0, forwards = 0, loves = 0 }) => {
+    const iconValueStype = {
+        fontSize: 16,
+        marginRight: 10,
+    };
+    return (
+        <div className="card-warp">
+            <Image
+                src={imgUrl}
+                width={150}
+                height={150}
+                fallback="https://pikaq-1257508274.cos.ap-shenzhen-fsi.myqcloud.com/123.png"
+            />
+            <div className="card-details-warp">
+                <div className="card-details-title">{title || '--'}</div>
+                <div className="card-details-subtitle">{subTitle || '--'}</div>
+                <div className="card-details-icons">
+                    <Statistic value={views} prefix={<FireOutlined />} valueStyle={iconValueStype} />
+                    <Statistic value={loves} prefix={<HeartOutlined />} valueStyle={iconValueStype} />
+                    <Statistic value={forwards} prefix={<DownloadOutlined />} valueStyle={iconValueStype} />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Card;
--- src/pages/home/article/index.tsx
+++ src/pages/home/article/index.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '@components/card';
+import { Button, Empty } from 'antd';
+import './index.less';
 
 const Article: React.FC = () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const [articleData, setArticleData] = useState<any[]>([]);
+    useEffect(() => {
+        onLoad();
+    }, []);
+    const onLoad = () => {
+        const data = ['v_vflqiu', 'qdsa', 'asd', 'a1eqw'].map((i) => ({
+            title: i,
+            subTitle: '1',
+            id: i,
+        }));
+        console.log(data);
+        setArticleData(data);
+    };
+
     return (
-        <div className="home-content-warp">
-            <h2>Article</h2>
-            <Card />
+        <div className="home-content-warp article-box">
+            {articleData.length === 0 ? (
+                <div className="empty-warp">
+                    <Empty description="暂无数据">
+                        <Button type="primary">Create Now</Button>
+                    </Empty>
+                </div>
+            ) : (
+                <div className="article-inner">
+                    <div className="article-list">
+                        {articleData.map((i) => (
+                            <Card imgUrl="" key={i.id} title={i.title} subTitle={i.subTitle} />
+                        ))}
+                    </div>
+                    <div className="article-top">1</div>
+                </div>
+            )}
         </div>
     );
 };
