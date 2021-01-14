d9cec084ab993bb0a89f46140ccafed674828bde
v_vflqiu <v_vflqiu@tencent.com>
2021-01-14 18:48:43 +0800

    Update md菜单项

--- src/pages/mdDetails/index.tsx
+++ src/pages/mdDetails/index.tsx
@@ -1,5 +1,5 @@
 import { Card } from 'antd';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Marked, { Renderer } from 'marked';
 import hljs from 'highlight.js';
 import Md from '../../../md/test.md';
@@ -17,12 +17,79 @@ Marked.setOptions({
     silent: true,
 });
 
-export default function MdDetails() {
+type NavData = { text: string; target: string; leval: number; paddingLeft: number }[];
+const baseMenuPaddingLeft = 20;
+
+const MdDetails: React.FC = () => {
+    const [navData, setNavData] = useState<NavData>([]);
+    const [hash, setHash] = useState(decodeURI(location.hash));
+    const contentRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        const parser = new DOMParser();
+        const dom = parser.parseFromString(Marked(Md), 'text/html');
+        const data: NavData = [];
+
+        Array.from(dom.body.children).map((i) => {
+            if (['h1', 'h2', 'h3', 'h4', 'h5', 'h5'].includes(i.tagName.toLocaleLowerCase())) {
+                const leval = +i.tagName.toLocaleLowerCase().split('h')[1];
+                data.push({
+                    leval,
+                    text: i.innerHTML,
+                    target: '#' + i.id,
+                    paddingLeft: (() => {
+                        const len = data.length;
+                        if (len === 0 || data[len - 1].leval > leval) return 0;
+                        if (data[len - 1].leval === leval) return data[len - 1].paddingLeft;
+                        return data[len - 1].paddingLeft + 20;
+                    })(),
+                });
+            }
+        });
+        setNavData(data);
+        // 监听滚动
+        contentScrollListen();
+    }, []);
+
+    useEffect(() => {
+        setHash(decodeURI(location.hash));
+    });
+
+    const contentScrollListen = () => {
+        console.log('ref');
+    };
+
     return (
-        <div className="home-content-warp misty">
+        <div className="home-content-warp vue md-details">
             <Card className="markdown-content">
-                <div dangerouslySetInnerHTML={{ __html: Marked(Md) }}></div>
+                <div
+                    dangerouslySetInnerHTML={{ __html: Marked(Md) }}
+                    ref={contentRef}
+                    onClick={() => {
+                        console.log(contentRef.current?.scrollTop);
+                    }}
+                ></div>
+            </Card>
+            <Card className="md-navigation">
+                <ul className="md-nav-ul">
+                    {navData.map((i) => (
+                        <li className={['md-nav-li', hash === i.target ? 'active' : ''].join(' ')} key={i.target}>
+                            <a
+                                href={i.target}
+                                title={i.text}
+                                className={!i.paddingLeft ? 'leval1' : ''}
+                                style={{
+                                    paddingLeft: !i.paddingLeft ? i.paddingLeft : baseMenuPaddingLeft,
+                                }}
+                            >
+                                {i.text}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
             </Card>
         </div>
     );
-}
+};
+
+export default MdDetails;
