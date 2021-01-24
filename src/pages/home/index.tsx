b5cef54a8dc8cdccef1ba4506034992c159a9a90
One-Lemon <qq982516861@gmail.com>
2021-01-24 18:10:27 +0800

    Update MD

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,9 @@
-import React, { ReactNode } from 'react';
+import React, { Fragment, ReactNode } from 'react';
 import { Button, Col, Empty, Row, Card, Progress, Statistic, Carousel } from 'antd';
 import './index.less';
 import { RouteComponentProps } from 'react-router-dom';
 import { keyToPath } from '@/utils/public';
+import { Footer } from 'antd/lib/layout/layout';
 
 interface IStates {
     data: string[];
@@ -30,86 +31,89 @@ export default class Home extends React.Component<IProps, IStates> {
         const { data } = this.state;
         const { history } = this.props;
         return (
-            <div className="home-content-warp home-box">
-                {data.length === 0 ? (
-                    <div className="empty-warp">
-                        <Empty description="暂无数据">
-                            <Button type="primary">刷新</Button>
-                        </Empty>
-                    </div>
-                ) : (
-                    <div className="home-inner">
-                        <Row gutter={40}>
-                            <Col span={16}>
-                                <div className="inner-card-left">
-                                    <div className="card-left-top">
-                                        <Card
-                                            className="card-left-mine-info"
-                                            title="List of Skills"
-                                            headStyle={{ border: 'none' }}
-                                        >
-                                            <Statistic title="Active Users" value={112893} />
-                                            <Progress percent={50} status="active" />
-                                            <Statistic title="Active Users" value={112893} />
-                                            <Progress percent={70} status="exception" />
-                                            <Statistic title="Active Users" value={112893} />
-                                            <Progress percent={100} />
-                                        </Card>
-                                        <Card
-                                            className="card-left-carousel"
-                                            title="asdad"
-                                            headStyle={{ border: 'none' }}
-                                        >
-                                            <Carousel className="carousel">
-                                                <div>
-                                                    <h3
-                                                        style={{
-                                                            height: 250,
-                                                            width: 600,
-                                                            backgroundColor: 'goldenrod',
-                                                        }}
-                                                    >
-                                                        1
-                                                    </h3>
-                                                </div>
-                                                <div>
-                                                    <h3
-                                                        style={{
-                                                            height: 250,
-                                                            width: 600,
-                                                            backgroundColor: 'greenyellow',
-                                                        }}
-                                                    >
-                                                        2
-                                                    </h3>
-                                                </div>
-                                            </Carousel>
-                                        </Card>
-                                    </div>
-                                    <div className="card-left-content">
-                                        <Card className="content-box">
-                                            <Button
-                                                onClick={() => {
-                                                    const href = keyToPath('MdDetails');
-                                                    history.push({
-                                                        pathname: '/mdDetails/12',
-                                                    });
-                                                    console.log(href);
-                                                }}
+            <Fragment>
+                <div className="home-content-warp home-box">
+                    {data.length === 0 ? (
+                        <div className="empty-warp">
+                            <Empty description="暂无数据">
+                                <Button type="primary">刷新</Button>
+                            </Empty>
+                        </div>
+                    ) : (
+                        <div className="home-inner">
+                            <Row gutter={40}>
+                                <Col span={16}>
+                                    <div className="inner-card-left">
+                                        <div className="card-left-top">
+                                            <Card
+                                                className="card-left-mine-info"
+                                                title="List of Skills"
+                                                headStyle={{ border: 'none' }}
                                             >
-                                                Detail
-                                            </Button>
-                                        </Card>
+                                                <Statistic title="Active Users" value={112893} />
+                                                <Progress percent={50} status="active" />
+                                                <Statistic title="Active Users" value={112893} />
+                                                <Progress percent={70} status="exception" />
+                                                <Statistic title="Active Users" value={112893} />
+                                                <Progress percent={100} />
+                                            </Card>
+                                            <Card
+                                                className="card-left-carousel"
+                                                title="asdad"
+                                                headStyle={{ border: 'none' }}
+                                            >
+                                                <Carousel className="carousel">
+                                                    <div>
+                                                        <h3
+                                                            style={{
+                                                                height: 250,
+                                                                width: 600,
+                                                                backgroundColor: 'goldenrod',
+                                                            }}
+                                                        >
+                                                            1
+                                                        </h3>
+                                                    </div>
+                                                    <div>
+                                                        <h3
+                                                            style={{
+                                                                height: 250,
+                                                                width: 600,
+                                                                backgroundColor: 'greenyellow',
+                                                            }}
+                                                        >
+                                                            2
+                                                        </h3>
+                                                    </div>
+                                                </Carousel>
+                                            </Card>
+                                        </div>
+                                        <div className="card-left-content">
+                                            <Card className="content-box">
+                                                <Button
+                                                    onClick={() => {
+                                                        const href = keyToPath('MdDetails');
+                                                        history.push({
+                                                            pathname: '/mdDetails/12',
+                                                        });
+                                                        console.log(href);
+                                                    }}
+                                                >
+                                                    Detail
+                                                </Button>
+                                            </Card>
+                                        </div>
                                     </div>
-                                </div>
-                            </Col>
-                            <Col span={8}>
-                                <Card className="inner-card-right">11</Card>
-                            </Col>
-                        </Row>
-                    </div>
-                )}
-            </div>
+                                </Col>
+                                <Col span={8}>
+                                    <Card className="inner-card-right">11</Card>
+                                </Col>
+                            </Row>
+                        </div>
+                    )}
+                </div>
+                <Footer>123</Footer>
+            </Fragment>
         );
     }
 }
